62ae02c04fd91ab2ea068f86fb85b15984031194
Aki Hirvonen <aki.hirvonen@loupedeck.com>
2022-10-04 16:10:05 +0300

    Update the demo plugin for Loupedeck 5.3 version

--- DemoPlugin/Step05/DemoPlugin/DemoPlugin.cs
+++ DemoPlugin/Step05/DemoPlugin/DemoPlugin.cs
@@ -4,27 +4,19 @@ namespace Loupedeck.DemoPlugin
 
     public class DemoPlugin : Plugin
     {
+        // Gets a value indicating whether this is an Universal plugin or an Application plugin.
+        public override Boolean UsesApplicationApiOnly => true;
+
+        // Gets a value indicating whether this is an API-only plugin.
+        public override Boolean HasNoApplication => true;
+
+        // This method is called when the plugin is loaded during the Loupedeck service start-up.
         public override void Load()
         {
         }
 
+        // This method is called when the plugin is unloaded during the Loupedeck service shutdown.
         public override void Unload()
-        {
-        }
-
-        private void OnApplicationStarted(Object sender, EventArgs e)
-        {
-        }
-
-        private void OnApplicationStopped(Object sender, EventArgs e)
-        {
-        }
-
-        public override void RunCommand(String commandName, String parameter)
-        {
-        }
-
-        public override void ApplyAdjustment(String adjustmentName, String parameter, Int32 diff)
         {
         }
     }
