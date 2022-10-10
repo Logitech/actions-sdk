45f1954cde3d155b0ca5081bc3d213123b8d50d7
Aki Hirvonen <aki.hirvonen@loupedeck.com>
2022-10-10 14:27:36 +0300

    Update the counter adjustment to match the generated skeleton plugin

--- DemoPlugin/Step06/DemoPlugin/CounterAdjustment.cs
+++ DemoPlugin/Step06/DemoPlugin/CounterAdjustment.cs
@@ -2,29 +2,35 @@
 {
     using System;
 
-    class CounterAdjustment : PluginDynamicAdjustment
+    // This class implements an example adjustment that counts the rotation ticks of a dial.
+
+    public class CounterAdjustment : PluginDynamicAdjustment
     {
+        // This variable holds the current value of the counter.
         private Int32 _counter = 0;
 
-        public CounterAdjustment() : base("Counter", "Counts rotation ticks", "Tools", true)
+        // Initializes the adjustment class.
+        // When `hasReset` is set to true, a reset command is automatically created for this adjustment.
+        public CounterAdjustment()
+            : base(displayName: "Counter", description: "Counts rotation ticks", groupName: "Tools", hasReset: true)
         {
         }
 
-        protected override void ApplyAdjustment(String actionParameter, Int32 ticks)
+        // This method is called when the dial associated to the plugin is rotated.
+        protected override void ApplyAdjustment(String actionParameter, Int32 diff)
         {
-            this._counter += ticks; // increase or decrease counter on the number of ticks
-            this.ActionImageChanged(actionParameter);
+            this._counter += diff; // Increase or decrease the counter by the number of ticks.
+            this.AdjustmentValueChanged(); // Notify the Loupedeck service that the adjustment value has changed.
         }
 
+        // This method is called when the reset command related to the adjustment is executed.
         protected override void RunCommand(String actionParameter)
         {
-            this._counter = 0; // reset counter to 0
-            this.ActionImageChanged(actionParameter);
+            this._counter = 0; // Reset the counter.
+            this.AdjustmentValueChanged(); // Notify the Loupedeck service that the adjustment value has changed.
         }
 
-        protected override String GetAdjustmentValue(String actionParameter)
-        {
-            return this._counter.ToString();
-        }
+        // Returns the adjustment value that is shown next to the dial.
+        protected override String GetAdjustmentValue(String actionParameter) => this._counter.ToString();
     }
 }
