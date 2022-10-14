141d798fe5a778a047aed1c5c059ec19bef0a164
Aki Hirvonen <aki.hirvonen@loupedeck.com>
2022-10-14 11:28:14 +0300

    LDSW-4794 Add plugin capabilities to the DemoPlugin class

--- DemoPlugin/Step01/DemoPlugin/DemoPlugin.cs
+++ DemoPlugin/Step01/DemoPlugin/DemoPlugin.cs
@@ -4,6 +4,12 @@ namespace Loupedeck.DemoPlugin
 
     public class DemoPlugin : Plugin
     {
+        // Gets a value indicating whether this is an Universal plugin or an Application plugin.
+        public override Boolean UsesApplicationApiOnly => true;
+
+        // Gets a value indicating whether this is an API-only plugin.
+        public override Boolean HasNoApplication => true;
+
         // This method is called when the plugin is loaded during the Loupedeck service start-up.
         public override void Load()
         {
